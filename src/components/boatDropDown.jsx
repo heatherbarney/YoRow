7b853983f07e0436352eb7924db1d355fbf472d2
Heather Barney <heatherbarney@Heathers-MacBook-Air.local>
2021-09-09 21:17:56 -0400

    Update file structure

---
+++ src/components/boatDropDown.jsx
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+
+class BoatDropdown extends Component {
+
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      showMenu: false,
+    }
+    
+    this.showMenu = this.showMenu.bind(this);
+  }
+  
+  showMenu(event) {
+    event.preventDefault();
+    
+    this.state.showMenu
+      ? (
+        this.setState({
+          showMenu: false,
+        })
+        )
+        : (
+          this.setState({
+            showMenu: true,
+          })
+        )
+    }
+
+  render() {
+
+    const buttons = [];
+    this.props.boatList.forEach(boat => {
+        buttons.push(
+          <button name={boat.name} id={boat.type} onClick={(event) => {this.props.chooseBoat(event); this.showMenu(event); this.props.clearLineup()}}>{boat.name + ' (' + boat.abbrev + ')'}</button>
+        )
+      });
+
+
+    return (
+      <div className="dropdown">
+        <button onClick={this.showMenu}>
+          Select Boat
+        </button>
+        
+        {
+          this.state.showMenu
+            ? (
+              <div className="menu">
+                {buttons}
+              </div>
+            )
+            : (
+              null
+            )
+        }
+      </div>
+    );
+  }
+}
+
+export default BoatDropdown;
