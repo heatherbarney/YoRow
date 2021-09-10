a893aeacf61ba1d660bbc7d570e2fdca8e0e018f
Heather Barney <heatherbarney@Heathers-MacBook-Air.local>
2021-09-09 22:24:21 -0400

    Fix file structure

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import MainContainer from './components/mainContainer.jsx';
+
+class App extends Component {
+  
+  render() {
+    return (
+      <div>
+        <MainContainer />
+      </div>
+    );
+  }
+}
+
+export default App;
